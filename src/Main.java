1453636b647573428ab15133a8910f587f637a0d
Sascha <sulai@gmx.at>
2015-05-25 19:37:36 +0200

    intial repo

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+import model.tutorial_00_specialResources1.ModelKasse;
+import desmoj.core.simulator.Experiment;
+import desmoj.core.simulator.TimeInstant;
+import desmoj.extensions.visualization2d.animation.CmdGeneration;
+import desmoj.extensions.visualization2d.engine.viewer.ViewerFrame;
+
+public class Main {
+	public static final String   	PATH_DATA				= 
+			new File("").getAbsolutePath() + File.separator + "model_data" + File.separator;
+	public static void main(String[] args) {
+		URL cmdFile;
+		URL iconDir;
+		CmdGeneration cmdGen;
+		try {
+			cmdFile = new File("").toURI().toURL();
+			iconDir = new File("resource").toURI().toURL();
+			cmdGen = new CmdGeneration("cmds/commands.cmds", "log", iconDir);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("error, aborting ...");
+			return;
+		}
+		// erzeuge Modell und Experiment
+		ModelKasse model = new ModelKasse(cmdGen);
+		Experiment exp = new Experiment("DriveIn");
+		cmdGen.setStartStopTime(new TimeInstant(0, TimeUnit.SECONDS), new TimeInstant(1, TimeUnit.SECONDS), TimeZone.getDefault());
+		model.connectToExperiment(exp);
+
+		// setze Experiment-Parameter
+		exp.setShowProgressBar(true);
+		System.out.println("start ...");// start
+																		// the
+																		// experiment
+		cmdGen.experimentStart(exp, 5.0);
+		// Beende CmdGeneration
+		cmdGen.close();
+//		 Nach dem Experiment: Gib Report aus und beende.
+		exp.report();
+		exp.finish();
+		ViewerFrame v = createViewer(cmdFile, iconDir);
+	}
+	public static ViewerFrame createViewer(URL cmdFile, URL imagePath){
+
+		ViewerFrame v = null;
+		try{
+			v = new ViewerFrame(cmdFile, imagePath, Locale.ENGLISH);
+			v.setLocation(0, 0);
+			v.setSize(800, 500);
+			v.getViewerPanel().setDefaultPath(PATH_DATA, PATH_DATA, PATH_DATA);
+			v.getViewerPanel().lastCall();
+		}catch(Exception e){
+			if(v != null)
+				v.getViewerPanel().setStatusMessage(e.getMessage());
+			e.printStackTrace();
+		}
+		return v;
+		
+	}
+}
